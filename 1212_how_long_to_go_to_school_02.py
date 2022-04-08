17465a327690948d79b78d33bf617bfcbb8514c8
dashakireeva <97792548+dashakireeva@users.noreply.github.com>
2022-04-08 22:59:14 +0500

    Add files via upload

---
+++ 1212_how_long_to_go_to_school_02.py
@@ -0,0 +1,28 @@
+# Сколько идти до школы?
+import requests
+from math import cos, sqrt
+from distance import lonlat_distance
+
+
+def w_w_w(toponym_to_find):
+    geocoder_api_server = "http://geocode-maps.yandex.ru/1.x/"
+    geocoder_params = {
+        "apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
+        "geocode": toponym_to_find,
+        "format": "json"}
+    response = requests.get(geocoder_api_server, params=geocoder_params)
+
+    if not response:
+        print("Ошибка выполнения запроса:")
+        print("Http статус:", response.status_code, "(", response.reason, ")")
+        sys.exit(1)
+
+    json_response = response.json()
+    toponym = json_response["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]
+    toponym_coodrinates = toponym["Point"]["pos"]
+    toponym_longitude, toponym_lattitude = toponym_coodrinates.split(" ")
+    return [float(toponym_longitude), float(toponym_lattitude)]
+
+x = input()
+y = input()
+print(int(lonlat_distance(w_w_w(x), w_w_w(y))), "метров")
